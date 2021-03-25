3002d264637ec3fa10a936535d469332b9946de5
haseeb <anwaarhaseeb@yahoo.com>
2021-03-25 14:44:52 +0500

    uikj

--- water.py
+++ water.py
@@ -1,10 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
 
-print("chnagded")
-import matplotlib.pyplot as plt
-import numpy as np
-x = np.linspace(0, 100)
-y = x * 0.4 + 10
+base = "https://papers.nips.cc"
+html = requests.get("https://papers.nips.cc/paper/2020").text
+soup = BeautifulSoup(html, 'html.parser')
 
-plt.yticks(range(0,600,50))
-plt.plot(x, y, 'x-')
-plt.show()
+data = [*soup.find_all('ul')[1].find_all('li')]
+data1 = [*soup.find_all('ul')[1].children]
+
+list_of_papers = []
+
+for item in data1:
+    if item != '\n':
+        title = item.find('a').text
+        url = base + item.find('a').get_attribute_list('href')[0]
+        html_abstract = requests.get(url).text
+        soup_abstract = BeautifulSoup(html_abstract, 'html.parser')
+        paragraphs = soup_abstract.find_all('p')
+        data_abstract = [text for text in paragraphs if len(text.text)>20]
+        list_of_papers.append({title:data_abstract})
+
+lists = []
+
+for x in list_of_papers:
+    for y,z in x.items():
+        lists.append([y,z[-1]])
+
+
+for y,z in list_of_papers[11].items():
+        print(y,z[-1])
+
+
+df = pd.DataFrame(lists, )
+
+# df.drop(index=[*range(10)], axis = 0, inplace  = True)
+df.to_csv('/aaaaaaaaaaaaaa.csv')
+# df = pd.read_csv("g:/aaaaaaaaaaaaaa.csv ")
+
+soup_abstract1 = BeautifulSoup(lists[12][1][-1], 'html.parser')
+paragraphs1 = soup_abstract.find_all('p')
+lists[10]
